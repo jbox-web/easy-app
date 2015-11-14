a8ff6ec2851153501c67d917a4871c0735c70d44
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-11-14 08:12:59 +0100

    Add translations for JQueryDataTables

---
+++ app/helpers/easy_app/data_tables_helper.rb
@@ -0,0 +1,32 @@
+module EasyAPP
+  module DataTablesHelper
+
+    def render_datatables_translations
+      data = {}
+      data[:processing]     = t('datatables.processing')
+      data[:search]         = t('datatables.search')
+      data[:lengthMenu]     = t('datatables.lengthMenu')
+      data[:info]           = t('datatables.info')
+      data[:infoEmpty]      = t('datatables.infoEmpty')
+      data[:infoFiltered]   = t('datatables.infoFiltered')
+      data[:infoPostFix]    = t('datatables.infoPostFix')
+      data[:loadingRecords] = t('datatables.loadingRecords')
+      data[:zeroRecords]    = t('datatables.zeroRecords')
+      data[:emptyTable]     = t('datatables.emptyTable')
+      data[:paginate] = {}
+      data[:paginate][:first]    = t('datatables.paginate.first')
+      data[:paginate][:previous] = t('datatables.paginate.previous')
+      data[:paginate][:next]     = t('datatables.paginate.next')
+      data[:paginate][:last]     = t('datatables.paginate.last')
+      data[:aria] = {}
+      data[:aria][:sortAscending]  = t('datatables.aria.sortAscending')
+      data[:aria][:sortDescending] = t('datatables.aria.sortDescending')
+      data[:buttons] = {}
+      data[:buttons][:pageLength] = {}
+      data[:buttons][:pageLength][:_]    = t('datatables.buttons.pageLength._')
+      data[:buttons][:pageLength][:'-1'] = t('datatables.buttons.pageLength.-1')
+      data.to_json
+    end
+
+  end
+end
