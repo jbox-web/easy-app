cfe9101d6e7a220b8c7d9d4e117a269c9d7e36cd
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-01-01 08:38:34 +0100

    Add helper method

--- app/helpers/easy_app/links_helper.rb
+++ app/helpers/easy_app/links_helper.rb
@@ -45,6 +45,13 @@ module EasyAPP
     end
 
 
+    def link_to_download(url, link_opts = {}, icon_opts = {})
+      label = link_opts.delete(:label) { t('button.download') }
+      icon  = icon_opts.delete(:icon) { 'fa-download' }
+      link_to_with_options(label, url, icon, link_opts, icon_opts)
+    end
+
+
     def link_to_delete(url, link_opts = {}, icon_opts = {})
       label  = link_opts.delete(:label) { t('button.delete') }
       button = link_opts.fetch(:button, false)
