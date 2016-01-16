34abd7482db0f3e01097462f7d2e1f121eb628ff
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-01-17 00:17:54 +0100

    Handle multi-select field for yadcf

--- app/presenters/easy_app/search_form_builder.rb
+++ app/presenters/easy_app/search_form_builder.rb
@@ -17,17 +17,32 @@ module EasyAPP
 
 
     def select(name, opts = {})
-      escape_strings = opts.delete(:escape_strings){ [] }
-      opts = opts.deep_merge(filter_type: 'select', filter_container_id: name, select_type: 'select2', select_type_options: { theme: 'bootstrap' })
-      @datatable.escape_strings(escape_strings)
-      @datatable.search_field(opts)
-      content_tag(:div, '', id: name)
+      opts = opts.deep_merge(filter_type: 'select')
+      select_field(name, opts)
+    end
+
+
+    def multi_select(name, opts = {})
+      opts = opts.deep_merge(filter_type: 'multi_select')
+      select_field(name, opts)
     end
 
 
     def render_datatable
       @datatable.render_datatable
     end
+
+
+    private
+
+
+      def select_field(name, opts = {})
+        escape_strings = opts.delete(:escape_strings){ [] }
+        opts = opts.deep_merge(filter_container_id: name, select_type: 'select2', select_type_options: { theme: 'bootstrap' })
+        @datatable.escape_strings(escape_strings)
+        @datatable.search_field(opts)
+        content_tag(:div, '', id: name)
+      end
 
   end
 end
