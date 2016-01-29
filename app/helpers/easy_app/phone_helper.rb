a3c5337a9729190e21b92aea4310f6818c443720
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-01-29 06:22:15 +0100

    Add link_to_phone method helper

--- app/helpers/easy_app/phone_helper.rb
+++ app/helpers/easy_app/phone_helper.rb
@@ -1,6 +1,12 @@
 module EasyAPP
   module PhoneHelper
 
+    def link_to_phone(phone, link_opts = {}, phone_opts = {})
+      return '' if phone.nil? || phone.empty?
+      link_to label_with_icon(format_phone(phone, phone_opts), 'fa-phone'), "tel:#{phone}", link_opts
+    end
+
+
     def format_phone(phone, opts = {})
       return '' if phone.nil?
       phone.phony_formatted(opts)
