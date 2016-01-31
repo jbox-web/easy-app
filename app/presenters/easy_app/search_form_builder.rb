d883f0d3f631978c4ef643d9b9839877fc4b6fdd
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-01-31 22:42:29 +0100

    Handle options for inputs container

--- app/presenters/easy_app/search_form_builder.rb
+++ app/presenters/easy_app/search_form_builder.rb
@@ -7,31 +7,33 @@ module EasyAPP
     end
 
 
-    def search_field(name, opts = {})
+    def search_field(name, opts = {}, container_opts = {})
       opts = opts.merge(filter_type: 'text', filter_container_id: name)
+      container_opts = container_opts.merge(id: name)
       escape_strings = opts.delete(:escape_strings){ [] }
       @datatable.escape_strings(escape_strings)
       @datatable.search_field(opts)
-      content_tag(:div, '', id: name)
+      content_tag(:div, '', container_opts)
     end
 
 
-    def select(name, opts = {})
+    def select(name, opts = {}, container_opts = {})
       opts = opts.deep_merge(filter_type: 'select')
-      select_field(name, opts)
+      select_field(name, opts, container_opts)
     end
 
 
-    def multi_select(name, opts = {})
+    def multi_select(name, opts = {}, container_opts = {})
       opts = opts.deep_merge(filter_type: 'multi_select')
-      select_field(name, opts)
+      select_field(name, opts, container_opts)
     end
 
 
-    def range_date(name, opts = {})
+    def range_date(name, opts = {}, container_opts = {})
       opts = opts.merge(filter_type: 'range_date', filter_container_id: name)
+      container_opts = container_opts.merge(id: name)
       @datatable.search_field(opts)
-      content_tag(:div, '', id: name)
+      content_tag(:div, '', container_opts)
     end
 
 
@@ -43,12 +45,13 @@ module EasyAPP
     private
 
 
-      def select_field(name, opts = {})
+      def select_field(name, opts = {}, container_opts = {})
         escape_strings = opts.delete(:escape_strings){ [] }
         opts = opts.deep_merge(filter_container_id: name, select_type: 'select2')
+        container_opts = container_opts.merge(id: name)
         @datatable.escape_strings(escape_strings)
         @datatable.search_field(opts)
-        content_tag(:div, '', id: name)
+        content_tag(:div, '', container_opts)
       end
 
   end
