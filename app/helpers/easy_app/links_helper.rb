dbce357160b2d4bde8bd890768d79732309ce430
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-02-06 19:36:03 +0100

    Add link_to_preview helper method

--- app/helpers/easy_app/links_helper.rb
+++ app/helpers/easy_app/links_helper.rb
@@ -45,6 +45,13 @@ module EasyAPP
     end
 
 
+    def link_to_preview(url, link_opts = {}, icon_opts = {})
+      label = link_opts.delete(:label) { t('button.preview') }
+      icon  = icon_opts.delete(:icon) { 'fa-eye' }
+      link_to_with_options(label, url, icon, link_opts, icon_opts)
+    end
+
+
     def link_to_download(url, link_opts = {}, icon_opts = {})
       label = link_opts.delete(:label) { t('button.download') }
       icon  = icon_opts.delete(:icon) { 'fa-download' }
