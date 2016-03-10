6804bccc192206ef33915f6147c1fcf61ade9ac7
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2016-03-10 03:02:07 +0100

    Extract method

--- app/helpers/easy_app/chartkick_helper.rb
+++ app/helpers/easy_app/chartkick_helper.rb
@@ -2,15 +2,21 @@ module EasyAPP
   module ChartkickHelper
 
     def render_chartkick_js
-      content = content_for(:charts_js)
-      content.gsub('<script type="text/javascript">', '')
-             .gsub('</script>', '')
-             .split("\n")
-             .select { |l| !l.blank? }
-             .map { |l| l.strip }
-             .join("\n")
-             .html_safe
+      content = clean_up_chartkick_js(content_for(:charts_js))
+      content.join("\n").html_safe
     end
+
+
+    private
+
+
+      def clean_up_chartkick_js(content)
+        content.gsub('<script type="text/javascript">', '')
+                     .gsub('</script>', '')
+                     .split("\n")
+                     .select { |l| !l.blank? }
+                     .map { |l| l.strip }
+      end
 
   end
 end
